102c2cc8b80929bcaa03abc531fc8f2261529152
ak4code <haritonov.aka@gmail.com>
2020-12-27 17:09:48 +0300

    front

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
+import UIkit from 'uikit'
+import '@/assets/styles/styles.scss'
+import Icons from 'uikit/dist/js/uikit-icons'
 
 Vue.config.productionTip = false
 
+UIkit.use(Icons)
+window.UIkit = UIkit
+
 new Vue({
-  el: '#app'
+  el: '#app',
+  components: {}
 })
